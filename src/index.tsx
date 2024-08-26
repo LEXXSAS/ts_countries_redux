6a0493e7c81fee4b9772272d6fde57d55050a93f
Alexey Sedov <ASedov15@gmail.com>
2024-08-26 22:33:15 +0300

    add hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as ReactDOMClient from "react-dom/client";
 import {Provider} from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
@@ -13,11 +13,11 @@ const rootElement = document.getElementById("root") as HTMLDivElement;
 const root = ReactDOMClient.createRoot(rootElement);
 
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
