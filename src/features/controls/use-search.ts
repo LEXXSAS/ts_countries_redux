1338c50f3e4060d24ccd7020b3fd548c315603cc
Alexey Sedov <ASedov15@gmail.com>
2024-08-26 22:16:27 +0300

    initial commit

---
+++ src/features/controls/use-search.ts
@@ -0,0 +1,17 @@
+import {useSelector} from 'react-redux';
+import { setSearch } from './controls-slice';
+import { selectSearch } from './controls-selectors';
+import { useAppDispatch } from 'store';
+
+type onSearch = React.ChangeEventHandler<HTMLInputElement>;
+
+export const useSearch = (): [string, onSearch] => {
+  const dispatch = useAppDispatch();
+  const search = useSelector(selectSearch);
+
+  const handleSearch: onSearch = (e) => {
+    dispatch(setSearch(e.target.value))
+  }
+
+  return [search, handleSearch];
+}
